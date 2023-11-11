f3bb8d52bf5c28894acf01e90923c9dc37965442
AlexSnipes <alejandro.sanchez.trek@gmail.com>
2023-11-11 15:59:35 -0300

    Health Test

---
+++ src/health/health.controller.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
+
+describe('HealthController', () => {
+  let controller: HealthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+      providers: [HealthService],
+    }).compile();
+
+    controller = module.get<HealthController>(HealthController);
+  });
+
+  describe('Check health', () => {
+    it('should return health', async () => {
+      const response = controller.getHealth();
+      expect(response.message).toEqual('Working');
+    });
+  });
+});
